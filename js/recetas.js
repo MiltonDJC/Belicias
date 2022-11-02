f0818e37bf39fe80a5aaa5a036e412b5dd0516e8
MiltonDJC <92831715+MiltonDJC@users.noreply.github.com>
2022-11-02 11:38:51 -0300

    Add files via upload

---
+++ js/recetas.js
@@ -0,0 +1,55 @@
+function recetas1(){
+    document.getElementById("receta1").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón.";
+    document.getElementById("receta1").style.fontFamily = "Arial";
+    document.getElementById("receta1").style.fontSize = "20px";
+    document.getElementById("receta1").style.fontWeight = "bolder";
+    // document.getElementById("boton1").innerHTML = "Cerrar receta";
+    document.getElementById('receta1').style.display = 'inline-block';
+}
+function ocultar1(){
+    document.getElementById("receta1").style.display = 'none';
+}
+function recetas2(){
+    document.getElementById("receta2").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón.";
+    document.getElementById("receta2").style.fontFamily = "Arial";
+    document.getElementById("receta2").style.fontSize = "20px";
+    document.getElementById("receta2").style.fontWeight = "bolder";
+    // document.getElementById("boton2").innerHTML = "Cerrar receta";
+    document.getElementById("receta2").style.display = 'block';
+}
+function ocultar2(){
+    document.getElementById("receta2").style.display = 'none';
+}
+function recetas3(){
+    document.getElementById("receta3").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón.";
+    document.getElementById("receta3").style.fontFamily = "Arial";
+    document.getElementById("receta3").style.fontSize = "20px";
+    document.getElementById("receta3").style.fontWeight = "bolder";
+    // document.getElementById("boton3").innerHTML = "Cerrar receta";
+    document.getElementById("receta3").style.display = 'block';
+}
+function ocultar3(){
+    document.getElementById("receta3").style.display = 'none';
+}
+function recetas4(){
+    document.getElementById("receta4").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón.";
+    document.getElementById("receta4").style.fontFamily = "Arial";
+    document.getElementById("receta4").style.fontSize = "20px";
+    document.getElementById("receta4").style.fontWeight = "bolder";
+    // document.getElementById("boton4").innerHTML = "Cerrar receta";
+    document.getElementById("receta4").style.display = 'block';
+}
+function ocultar4(){
+    document.getElementById("receta4").style.display = 'none';
+}
+function recetas5(){
+    document.getElementById("receta5").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón.";
+    document.getElementById("receta5").style.fontFamily = "Arial";
+    document.getElementById("receta5").style.fontSize = "20px";
+    document.getElementById("receta5").style.fontWeight = "bolder";
+    // document.getElementById("boton5").innerHTML = "Cerrar receta";
+    document.getElementById("receta5").style.display = 'block';
+}
+function ocultar5(){
+    document.getElementById("receta5").style.display = 'none';
+}
