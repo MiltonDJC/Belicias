388d5470e995e5e2123ba7477d510a9f1c37879f
MiltonDJC <miltonaxe2001@gmail.com>
2022-11-04 10:23:35 -0300

    cambios enviados

--- js/recetas.js
+++ js/recetas.js
@@ -1,54 +1,71 @@
 function recetas1(){
-    document.getElementById("receta1").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón.";
-    document.getElementById("receta1").style.fontFamily = "Arial";
-    document.getElementById("receta1").style.fontSize = "20px";
-    document.getElementById("receta1").style.fontWeight = "bolder";
-    // document.getElementById("boton1").innerHTML = "Cerrar receta";
+    document.getElementById("receta1").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón. <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro dolor ab ducimus commodi ex non molestiae illum incidunt illo architecto veniam, modi doloremque ratione! Labore quod numquam illum iusto eos.";
+    document.getElementById("receta1").style.backgroundColor="#fff";
+    document.getElementById("receta1").style.border="2px solid red";
+    document.getElementById("receta1").style.borderRadius="10px";
+    document.getElementById("receta1").style.padding="10px";
+    document.getElementById("receta1").style.margin="10px 0";
+    document.getElementById("receta1").style.animationDuration="1s";
+    document.getElementById("receta1").style.animationName="slidein";
     document.getElementById('receta1').style.display = 'inline-block';
 }
 function ocultar1(){
-    document.getElementById("receta1").style.display = 'none';
+    // document.getElementById("receta1").style.display = 'none';
+    document.getElementById("receta1").style.animationDuration="1s";
+    document.getElementById("receta1").style.display="none";
 }
 function recetas2(){
-    document.getElementById("receta2").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón.";
-    document.getElementById("receta2").style.fontFamily = "Arial";
-    document.getElementById("receta2").style.fontSize = "20px";
-    document.getElementById("receta2").style.fontWeight = "bolder";
-    // document.getElementById("boton2").innerHTML = "Cerrar receta";
-    document.getElementById("receta2").style.display = 'block';
+    document.getElementById("receta2").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón. <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro dolor ab ducimus commodi ex non molestiae illum incidunt illo architecto veniam, modi doloremque ratione! Labore quod numquam illum iusto eos.";
+    document.getElementById("receta2").style.backgroundColor="#fff";
+    document.getElementById("receta2").style.border="2px solid red";
+    document.getElementById("receta2").style.borderRadius="10px";
+    document.getElementById("receta2").style.padding="10px";
+    document.getElementById("receta2").style.margin="10px 0";
+    document.getElementById("receta2").style.animationDuration="1s";
+    document.getElementById("receta2").style.animationName="slidein";
+    document.getElementById('receta2').style.display = 'inline-block';
 }
 function ocultar2(){
     document.getElementById("receta2").style.display = 'none';
 }
 function recetas3(){
-    document.getElementById("receta3").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón.";
-    document.getElementById("receta3").style.fontFamily = "Arial";
-    document.getElementById("receta3").style.fontSize = "20px";
-    document.getElementById("receta3").style.fontWeight = "bolder";
-    // document.getElementById("boton3").innerHTML = "Cerrar receta";
-    document.getElementById("receta3").style.display = 'block';
+    document.getElementById("receta3").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón. <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro dolor ab ducimus commodi ex non molestiae illum incidunt illo architecto veniam, modi doloremque ratione! Labore quod numquam illum iusto eos.";
+    document.getElementById("receta3").style.backgroundColor="#fff";
+    document.getElementById("receta3").style.border="2px solid red";
+    document.getElementById("receta3").style.borderRadius="10px";
+    document.getElementById("receta3").style.padding="10px";
+    document.getElementById("receta3").style.margin="10px 0";
+    document.getElementById("receta3").style.animationDuration="1s";
+    document.getElementById("receta3").style.animationName="slidein";
+    document.getElementById('receta3').style.display = 'inline-block';
 }
 function ocultar3(){
     document.getElementById("receta3").style.display = 'none';
 }
 function recetas4(){
-    document.getElementById("receta4").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón.";
-    document.getElementById("receta4").style.fontFamily = "Arial";
-    document.getElementById("receta4").style.fontSize = "20px";
-    document.getElementById("receta4").style.fontWeight = "bolder";
-    // document.getElementById("boton4").innerHTML = "Cerrar receta";
-    document.getElementById("receta4").style.display = 'block';
+    document.getElementById("receta4").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón. <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro dolor ab ducimus commodi ex non molestiae illum incidunt illo architecto veniam, modi doloremque ratione! Labore quod numquam illum iusto eos.";
+    document.getElementById("receta4").style.backgroundColor="#fff";
+    document.getElementById("receta4").style.border="2px solid red";
+    document.getElementById("receta4").style.borderRadius="10px";
+    document.getElementById("receta4").style.padding="10px";
+    document.getElementById("receta4").style.margin="10px 0";
+    document.getElementById("receta4").style.animationDuration="1s";
+    document.getElementById("receta4").style.animationName="slidein";
+    document.getElementById('receta4').style.display = 'inline-block';
 }
 function ocultar4(){
     document.getElementById("receta4").style.display = 'none';
 }
 function recetas5(){
-    document.getElementById("receta5").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón.";
-    document.getElementById("receta5").style.fontFamily = "Arial";
-    document.getElementById("receta5").style.fontSize = "20px";
-    document.getElementById("receta5").style.fontWeight = "bolder";
-    // document.getElementById("boton5").innerHTML = "Cerrar receta";
-    document.getElementById("receta5").style.display = 'block';
+    document.getElementById("receta5").innerHTML = "Aquí es donde se encontraría la receta del producto que se ve encima del botón. <br><br> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro dolor ab ducimus commodi ex non molestiae illum incidunt illo architecto veniam, modi doloremque ratione! Labore quod numquam illum iusto eos.";
+    document.getElementById("receta5").style.backgroundColor="#fff";
+    document.getElementById("receta5").style.border="2px solid red";
+    document.getElementById("receta5").style.borderRadius="10px";
+    document.getElementById("receta5").style.padding="10px";
+    document.getElementById("receta5").style.margin="10px 0";
+    document.getElementById("receta5").style.animationDuration="1s";
+    document.getElementById("receta5").style.animationName="slidein";
+    document.getElementById('receta5').style.display = 'inline-block';
 }
 function ocultar5(){
     document.getElementById("receta5").style.display = 'none';
